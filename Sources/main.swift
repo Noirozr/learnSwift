b8a486181727812605265ab520096885e78a3086
10917 <yongjia.liu01@hand-china.com>
2017-03-15 00:07:55 +0800

    add 1 hour learning result

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,46 @@
-print("Hello, world!")
+import Foundation
+import CommandLineKit
+import Rainbow 
+
+let cli = CommandLineKit.CommandLine()
+
+let projectOption = StringOption(shortFlag: "p", longFlag: "project",
+                               helpMessage: "Path to the project.")
+let resourceExtensionOption = MultiStringOption(shortFlag: "r", longFlag: "resource-extensions",
+                          helpMessage: "Extensions to search.")
+
+let fileExtensionsOption = MultiStringOption(shortFlag: "f", longFlag: "file-extensions", helpMessage: "file-extensions to search")
+let help = BoolOption(shortFlag: "h", longFlag: "help",
+                      helpMessage: "Prints a help message.")
+
+cli.addOptions(projectOption, resourceExtensionOption, fileExtensionsOption, help)
+
+cli.formatOutput = { s, type in
+    var str: String
+    switch(type) {
+    case .error:
+        str = s.red.bold
+    case .optionFlag:
+        str = s.green.underline
+    case .optionHelp:
+        str = s.lightGreen
+    default:
+        str = s
+    }
+    
+    return cli.defaultFormat(s:str, type: type)
+}
+
+do {
+    try cli.parse()
+} catch {
+    cli.printUsage(error)
+    exit(EX_USAGE)
+}
+
+if help.value {
+    cli.printUsage()
+    exit(EX_OK)
+}
+
+print("Called!".red)
