bbcc637667075a86d6648a0b398567df0a59eb90
LiebromLiu <noirozr@icloud.com>
2017-03-17 00:54:00 +0800

    finish video part

--- Sources/LearnSwiftKit/LearnSwiftKit.swift
+++ Sources/LearnSwiftKit/LearnSwiftKit.swift
@@ -25,6 +25,7 @@ public struct LearnSwift {
     }
     
     func stringsInUse() -> [String] {
+        
         return []
     }
     
---
+++ Sources/LearnSwiftKit/StringSearchRule.swift
@@ -0,0 +1,42 @@
+//
+//  StringSearchRule.swift
+//  learnSwift
+//
+//  Created by Yongjia Liu on 2017/3/17.
+//
+//
+
+import Foundation
+
+
+protocol StringsSearcher {
+    func search(in content: String) -> Set<String>
+}
+
+protocol RegexStringsSearcher: StringsSearcher {
+    var patterns: [String] { get }
+}
+
+extension RegexStringsSearcher {
+    func search(in content: String) -> Set<String> {
+        
+        var result = Set<String>()
+        
+        for pattern in patterns {
+            guard let regex = try? NSRegularExpression(pattern: pattern, options: []) else {
+                print("Failed to craete regular expression.")
+                continue
+            }
+            let matches = regex.matches(in: content, options: [], range: content.fullRange)
+            for checkingResult in matches {
+                let range = checkingResult.rangeAt(1)
+                let extracted = NSString(string: content).substring(with: range)
+                 result.insert(extracted.plainName)
+            }
+        }
+        
+        return result
+    }
+}
+
+
